12b0bba10b0bf1d02f863ac73f94c9d028e4976f
unknown <yravi.ui@gmail.com>
2024-04-03 23:27:07 +0530

    User apis

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import colors from 'colors'
 import dotenv from 'dotenv'
 import cors from 'cors'
 import connectDB from './config/db.js'
+import userRoutes from './routes/userRoute.js'
 
 // PORT
 const PORT = process.env.PORT || 8080
@@ -26,6 +27,9 @@ app.use(express.json())
 app.get('/api', (req, res) =>{
     res.send({ message: 'Welcome to FS App - API Testing'})
 })
+
+// all users
+app.use('/api/v1/', userRoutes) 
 
 // run listen
 app.listen(PORT, () => console.log(`Server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white))
